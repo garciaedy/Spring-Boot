cdc4ec5437eff37f10e0f0b26311b07a122db7fd
garciaedy <edygarcia97@gmail.com>
2024-03-11 00:31:57 -0400

    working project

---
+++ UserRegistration/src/main/java/com/garcia/userregistration/service/StudentService.java
@@ -0,0 +1,18 @@
+package com.garcia.userregistration.service;
+
+import java.util.List;
+
+import com.example.Barebones.model.Student;
+
+
+public interface StudentService {
+	List<Student> getAllStudents();
+	
+	Student saveStudent(Student student);
+	
+	Student getStudentById(Long id);
+	
+	Student updateStudent(Student student);
+	
+	void deleteStudentById(Long id);
+}
